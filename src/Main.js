95fd599cd9515b4947d16abc83b6cb043538b57f
jeevan <jeevanhrx@gmail.com>
2024-03-21 15:43:36 +0530

    check uncheck funcion handled for checkboxes

--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,43 @@
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 let Items = [
   { id: 1, checked: false, item: "Item 1" },
-  { id: 2, checked: false, item: "Item 2" },
+  { id: 2, checked: true, item: "Item 2" },
   { id: 3, checked: false, item: "Item 3" },
 ];
 
 function Main() {
   let [items, setItems] = useState(Items);
 
+  function handleChange(id) {
+    let newItems = items.map((item) => {
+      if (Number(id) === item.id) {
+        if (item.checked) item.checked = false;
+        else item.checked = true;
+      }
+      return item;
+    });
+    setItems(newItems);
+  }
+
   return (
     <main>
       <ul>
-        {items.map((ele) => {
-          const { id, item, checked } = ele;
+        {items.map((el) => {
+          const { id, item, checked } = el;
           return (
-            <li>
-              <input type="checkbox" id={id} key={id} checked={checked} />
+            <li className="item" key={id}>
+              <input
+                type="checkbox"
+                id={id}
+                checked={checked}
+                onChange={(e) => {
+                  handleChange(id);
+                }}
+              />
               <label htmlFor={id}>{item}</label>
-              <button>Delete</button>
+              <FaTrashAlt role="button" tabIndex="0" />
             </li>
           );
         })}
